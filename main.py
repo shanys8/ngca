bcce68337df1c448e0c6bb0c2006f4d103004383
Shany Shmuely <shany.shmuely@ironsrc.com>
2019-08-13 16:19:38 +0300

    implement synthetic data, check whitening function

---
+++ main.py
@@ -0,0 +1,66 @@
+from ngca_theoretical_algorithm import run as run_ngca_theoretical_algorithm
+import utilities
+import numpy as np
+
+
+def generate_synthetic_samples(number_of_samples, n, k, m, type_of_requested_subspace):
+
+    samples = np.empty((n, 0), float)
+    samples_copy = np.empty((n, 0), float)
+
+    B = np.random.rand(n, n)
+    Q, _ = np.linalg.qr(B)
+    Q_E = Q[:, 0:k]
+    Q_E_orth = Q[:, k:]
+
+    # Gaussian subspace to sample - stem from Q_E_orth
+    G_initial = np.random.randn(n-k, m)
+    G_to_sample = np.dot(Q_E_orth, G_initial)
+
+    # subspace to sample - stem from Q_E
+    if type_of_requested_subspace == 'gaussian':
+        G_X_initial = np.random.randn(k, m)
+        X_to_sample = np.dot(Q_E, G_X_initial)
+    else:
+        raise ValueError('unsupported type of requested subspace', type_of_requested_subspace)
+
+    # samples generated as direct sum of range(G_to_sample) and range(X_to_sample)
+    for _ in range(number_of_samples):
+        # each sample should have mean zero
+        sample = np.dot(G_to_sample, np.random.randn(m, 1)) + np.dot(X_to_sample, (np.random.rand(m, 1) - 0.5))
+        samples = np.append(samples, sample, axis=1)
+
+    for _ in range(number_of_samples):
+        # each sample should have mean zero
+        sample_copy = np.dot(G_to_sample, np.random.randn(m, 1)) + np.dot(X_to_sample, (np.random.rand(m, 1) - 0.5))
+        samples_copy = np.append(samples_copy, sample_copy, axis=1)
+
+    return samples, samples_copy, Q_E
+
+def main():
+
+    m = 5
+    n = 10  # dimension
+    k = 3  # NG subspace dimension
+    number_of_samples = 10
+    type_of_requested_subspace = 'gaussian'  # uniform, gaussian, super_gaussian
+
+    alpha1 = 1
+    alpha2 = 1
+    beta1 = 1
+    beta2 = 1
+
+    samples, samples_copy, Q_E = generate_synthetic_samples(number_of_samples, n, k, m, type_of_requested_subspace)
+
+    # E is the range of Q_E
+
+    # Implementation of algorithm in the paper
+    approx_ng_subspace = run_ngca_theoretical_algorithm(samples, samples_copy, alpha1, alpha2, beta1, beta2)
+
+    # TODO compare E and approx_ng_subspace
+
+    return approx_ng_subspace
+
+
+if __name__ == "__main__":
+    main()
---
+++ ngca_theoretical_algorithm.py
@@ -0,0 +1,143 @@
+import numpy as np
+import math
+from numpy import linalg as LA
+
+
+def compute_matrix_phi(samples, samples_copy, alpha):
+    result = 0
+    z_phi_coeficient = (1 / compute_z_phi(samples, alpha))
+    for sample in samples:
+        result += math.exp((-1) * alpha * math.pow(LA.norm(sample), 2)) * \
+                  np.dot(sample[:, np.newaxis], sample[np.newaxis, :])
+
+    return z_phi_coeficient * result
+
+
+def compute_z_phi(samples, alpha):
+    return np.array([(math.exp((-1) * alpha * math.pow(LA.norm(sample), 2))) for sample in samples]).sum()
+
+
+def compute_matrix_psi(samples, samples_copy, alpha):
+    return_val = 0
+    i = 0
+    while i < samples.shape[0]:
+        return_val += math.exp((-1) * alpha * np.dot(samples[i][np.newaxis, :], samples_copy[i][:, np.newaxis])) * \
+                      ((np.dot(samples[i][:, np.newaxis], samples_copy[i][np.newaxis, :])) +
+                       (np.dot(samples_copy[i][:, np.newaxis], samples[i][np.newaxis, :])))
+        i += 1
+
+    return (1 / compute_z_psi(samples, samples_copy, alpha)) * return_val
+
+
+def compute_z_psi(samples, samples_copy, alpha):
+    return_val = 0
+    i = 0
+    while i < samples.shape[0]:
+        return_val += math.exp((-1) * alpha * np.dot(samples[i][np.newaxis, :], samples_copy[i][:, np.newaxis]))
+        i += 1
+
+    return 2 * return_val
+
+
+def get_matrix_relevant_eigenvalues(matrix_eigenvalues, gaussian_eigenvalue, threshold):
+    return np.where(math.fabs(matrix_eigenvalues - gaussian_eigenvalue) > threshold)
+
+
+def calculate_gaussian_phi_eigenvalue(alpha):
+    return math.pow(2 * alpha + 1, -1)
+
+
+def calculate_gaussian_psi_eigenvalue(alpha):
+    return alpha * math.pow(alpha * alpha - 1, -1)
+
+
+def get_matrix_relevant_eigenvectors(matrix, gaussian_eigenvalue, threshold):
+    min_eigenvalue_to_count = 1e-5
+    eigenvalues, eigenvectors = LA.eigh(matrix)
+    if eigenvectors.dtype.name != 'float64':
+        raise ValueError('eigenvalues are not all floats')
+    relevant_eigenvectors = np.empty((eigenvectors.shape[0], 0), float)
+    i = 0
+    while i < len(eigenvalues):
+        if eigenvalues[i] > min_eigenvalue_to_count and math.fabs(eigenvalues[i] - gaussian_eigenvalue) > threshold:
+            relevant_eigenvectors = np.append(relevant_eigenvectors, eigenvectors[:, i][:, np.newaxis], axis=1)
+        i += 1
+
+    return relevant_eigenvectors
+
+
+def union_subspace(e1, e2):
+    return np.concatenate((e1, e2), axis=1)
+
+
+def center(X):
+    c = np.mean(X, axis = 0)
+    Xw = X - c
+    return Xw, c
+
+
+def whiten(X):
+    X_c, c = center(X)
+    U, s, VT = np.linalg.svd(X_c, full_matrices=False)
+    W = VT.T * (math.sqrt(X.shape[0]) / s)
+    X_w = U * math.sqrt(X.shape[0])
+    return X_w, c, W
+
+
+def assert_isotropic_model(X):
+    assert (np.allclose(np.mean(X, axis=0), np.zeros(X.shape[1]), rtol=1.e-2,
+                        atol=1.e-2))  # each column vector should have mean zero
+    cov_X = np.cov(X, rowvar=False, bias=True)
+    assert (cov_X.shape[0] == cov_X.shape[1]) and np.allclose(cov_X, np.eye(cov_X.shape[0]), rtol=1.e-1,
+                                                              atol=1.e-1)  # covariance matrix should by identity
+
+
+def assert_all_columns_unit_vectors(matrix):
+    i = 0
+    while i < matrix.shape[1]:
+        assert (is_unit_vector(matrix[:, i][:, np.newaxis]))
+        i += 1
+
+
+def is_unit_vector(vector):
+    return math.isclose(LA.norm(vector),  1.0, rel_tol=1e-2)
+
+
+def run_ngca_algorithm(samples, samples_copy, alpha1, alpha2, beta1, beta2):
+
+    # Whiten samples
+    samples, c_samples, W_samples = whiten(samples)
+    samples_copy, c_samples_copy, W_samples_copy = whiten(samples_copy)
+
+    assert_isotropic_model(samples)
+    assert_isotropic_model(samples_copy)
+
+    # Calculate matrices
+    matrix_phi = compute_matrix_phi(samples, samples_copy, alpha1)
+
+    matrix_psi = compute_matrix_psi(samples, samples_copy, alpha2)
+
+    # Calculate the gaussian eigenvalue for each matrix
+    gaussian_phi_eigenvalue = calculate_gaussian_phi_eigenvalue(alpha1)
+    gaussian_psi_eigenvalue = calculate_gaussian_psi_eigenvalue(alpha2)
+
+    # Calculate corresponding eigenvectors for the relevant eigenvalues -
+    # those which are far away beta from the gaussian eigenvalues
+    matrix_phi_relevant_eigenvectors = get_matrix_relevant_eigenvectors(matrix_phi,
+                                                                        gaussian_phi_eigenvalue, beta1)
+    matrix_psi_relevant_eigenvectors = get_matrix_relevant_eigenvectors(matrix_psi,
+                                                                        gaussian_psi_eigenvalue, beta2)
+
+    # Calculate E space - non gaussian space
+    result_space = union_subspace(matrix_phi_relevant_eigenvectors, matrix_psi_relevant_eigenvectors)
+
+    assert_all_columns_unit_vectors(result_space)
+
+    return result_space
+
+
+def run(samples, samples_copy, alpha1, alpha2, beta1, beta2):
+    # Implementation of algorithm in the paper
+    approx_ng_subspace = run_ngca_algorithm(samples, samples_copy, alpha1, alpha2, beta1, beta2)
+    return approx_ng_subspace
+
